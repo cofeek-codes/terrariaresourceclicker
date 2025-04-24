42133a4673622a3f0c0efe2dd61261b2b92bfc75
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-24 05:34:20 +0500

    cursor grouped + block collision detection

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,35 +2,50 @@ extends Node2D
 
 @export var player_data: PlayerData
 
-@onready var cursor: Sprite2D = %Cursor
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var pickaxe_audio_player: AudioStreamPlayer = $PickaxeAudioPlayer
+# cursor
+@onready var cursor: Node2D = %Cursor
+@onready var cursor_area: Area2D = %Cursor/CursorArea
+@onready var cursor_sprite: Sprite2D = %Cursor/CursorArea/CursorSprite
+@onready var cursor_animation_player: AnimationPlayer = %Cursor/CursorAnimationPlayer
+@onready var cursor_audio_player: AudioStreamPlayer = %Cursor/CursorAudioPlayer
+
+# cursor
+
+
+@onready var block: Node2D = %Block
+@onready var block_area: Area2D = block.get_node('BlockArea')
+
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-	cursor.texture = player_data.get_pickaxe_texture()
+	cursor_sprite.texture = player_data.get_pickaxe_texture()
+	block.position = Vector2(get_viewport_rect().size.x / 2, get_viewport_rect().size.y / 2)
 	
 
 func handle_click():
 	print('click')
-	pickaxe_audio_player.pitch_scale = randf_range(0.5, 1.5)
-	pickaxe_audio_player.play()
-	animation_player.stop()
-	if is_cursor_right():
-		animation_player.play("click_right")
-		animation_player.play_backwards("click_right")
-	else:
-		animation_player.play("click_left")
-		animation_player.play_backwards("click_left")
+	cursor_audio_player.pitch_scale = randf_range(0.5, 1.5)
+	cursor_audio_player.play()
 	
+	cursor_animation_player.stop()
+	if is_cursor_right():
+		cursor_animation_player.play("click_right")
+		cursor_animation_player.play_backwards("click_right")
+	else:
+		cursor_animation_player.play("click_left")
+		cursor_animation_player.play_backwards("click_left")
+		
+# click on block
+	if cursor_area in block_area.get_overlapping_areas():
+		block.handle_click()
 	
 func is_cursor_right():
 	var screen_center = get_viewport_rect().size.x / 2
 	return get_global_mouse_position().x > screen_center
 	
 func _process(delta: float) -> void:
-	cursor.flip_h = is_cursor_right()
+	cursor_sprite.flip_h = is_cursor_right()
 	cursor.position = get_global_mouse_position()
 	if Input.is_action_just_pressed('click'):
 		handle_click()
