e1626e21834d7e8528d0144666ba509a7e45e245
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-25 07:24:32 +0500

    working on damaging block + damage sound applied

--- scripts/block.gd
+++ scripts/block.gd
@@ -1,22 +1,23 @@
 extends Node2D
 
-var tween: Tween
-
 @export var block_data: BlockData
 
 @onready var sprite: Sprite2D = $BlockArea/Sprite
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var block_hit_particles: GPUParticles2D = $BlockHitParticles
+@onready var hit_audio_player: AudioStreamPlayer = $HitAudioPlayer
 
 @onready var cursor = $"../Cursor"
 
 const TWEEN_DURATION: float = 0.5
 const MAX_PARTICLES: int = 50
 
-
+var tween: Tween
+var current_hp
 
 func _ready() -> void:
 	self.position = Vector2(get_viewport_rect().size.x / 2, get_viewport_rect().size.y / 2)
+	current_hp = block_data.health
 	
 	# apply blockdata
 	sprite.texture = block_data.texture
@@ -26,7 +27,19 @@ func _ready() -> void:
 func handle_click():
 	print('clicked on block')
 	apply_click_visuals()
+	apply_block_hit()
 
+
+func apply_block_hit():
+	# @TODO: apply proper damage
+	current_hp -= 1
+	if current_hp <= 0:
+		apply_block_destroy()
+	print(current_hp) 
+	
+	
+func apply_block_destroy():
+	print('block should be destroyed')
 
 func apply_click_visuals():
 	shake()
@@ -36,6 +49,8 @@ func shake():
 	#var tween = Utils.safe_create_tween(tween)
 	#tween.tween_property(sprite, "scale", Vector2.ONE * 0.5, TWEEN_DURATION)
 	#tween.tween_property(sprite, "scale", Vector2.ONE, TWEEN_DURATION)
+	hit_audio_player.pitch_scale = randf_range(0.8, 1)
+	hit_audio_player.play()
 	animation_player.stop()
 	if cursor.is_cursor_right():
 		animation_player.play("hit_right")
