353a5d4ac22f3976fdf61a48b5d9ebc21cee0d0c
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-26 21:49:56 +0500

    remove unneded util

--- scripts/singletones/autoloads/utils.gd
+++ scripts/singletones/autoloads/utils.gd
@@ -1,8 +1,1 @@
 extends Node
-
-
-func safe_create_tween(tween: Tween, trans_type: Tween.TransitionType = Tween.TransitionType.TRANS_CUBIC) -> Tween:
-	if tween == null || !tween.is_running():
-		tween = get_tree().create_tween().set_trans(trans_type)
-		
-	return tween
