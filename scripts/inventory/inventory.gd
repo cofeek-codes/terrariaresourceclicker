ed8d10dc91f1d5ddf346906e30ccd28ded04be96
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-28 03:49:10 +0500

    inventory open logic

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -1,6 +1,19 @@
 extends Control
 
+signal inventory_open
+signal inventory_close
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
+	pass
+
+
+func _on_inventory_open() -> void:
+	print('inventory_open signal recieved')
 	animation_player.play("appear")
+
+
+func _on_inventory_close() -> void:
+	print('inventory_close signal recieved')
+	animation_player.play_backwards("appear")
--- scripts/inventory/inventory_button.gd
+++ scripts/inventory/inventory_button.gd
@@ -1,6 +1,16 @@
-extends PanelContainer
+extends Control
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var inventory: Control = $"../Inventory"
 
 func _ready() -> void:
 	pass
+
+
+func play_disappear_animation():
+	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "position:x", -78, 0.3)
+
+func _on_open_button_pressed() -> void:
+	print('inventory button pressed')
+	play_disappear_animation()
+	inventory.emit_signal('inventory_open')
