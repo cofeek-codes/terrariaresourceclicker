3d32b7852c70a0b41f285a1b95e5672f701922b0
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-29 02:52:12 +0500

    inventory now can be closed and opened

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -1,9 +1,10 @@
 extends Control
 
 signal inventory_open
-signal inventory_close
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var inventory_button: Control = $"../InventoryButton"
 
 func _ready() -> void:
 	pass
@@ -14,6 +15,11 @@ func _on_inventory_open() -> void:
 	animation_player.play("appear")
 
 
-func _on_inventory_close() -> void:
+func close_inventory() -> void:
 	print('inventory_close signal recieved')
 	animation_player.play_backwards("appear")
+	inventory_button.emit_signal('inventory_close')
+
+
+func _on_close_button_pressed() -> void:
+	close_inventory()
--- scripts/inventory/inventory_button.gd
+++ scripts/inventory/inventory_button.gd
@@ -1,10 +1,16 @@
 extends Control
 
+signal inventory_close
+
 @onready var inventory: Control = $"../Inventory"
 
 func _ready() -> void:
 	pass
 
+
+func play_appear_animation():
+	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "position:x", 0, 0.3)
 
 func play_disappear_animation():
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
@@ -14,3 +20,8 @@ func _on_open_button_pressed() -> void:
 	print('inventory button pressed')
 	play_disappear_animation()
 	inventory.emit_signal('inventory_open')
+
+
+func _on_inventory_close() -> void:
+	print('_on_inventory_close signal recieved')
+	play_appear_animation()
