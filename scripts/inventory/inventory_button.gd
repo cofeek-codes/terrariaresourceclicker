82464ef24542eafb99ce8935a01078bfa0c18461
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-29 02:58:08 +0500

    formatting

--- scripts/inventory/inventory_button.gd
+++ scripts/inventory/inventory_button.gd
@@ -7,14 +7,15 @@ signal inventory_close
 func _ready() -> void:
 	pass
 
-
 func play_appear_animation():
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, "position:x", 0, 0.3)
 
+
 func play_disappear_animation():
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, "position:x", -78, 0.3)
+
 
 func _on_open_button_pressed() -> void:
 	print('inventory button pressed')
