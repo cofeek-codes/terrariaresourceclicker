0bcd82f1f206cf00bdc5b1dd27fb63f8d6f8de55
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-05-05 00:30:44 +0500

    shop_item: buy_button disabled

--- scripts/shop/shop_item.gd
+++ scripts/shop/shop_item.gd
@@ -1,10 +1,13 @@
 extends PanelContainer
 
+@onready var player_data: PlayerData = Globals.get_player_data()
+
 @export var item: ShopItem
 
 @onready var item_image: TextureRect = %ItemImage
 @onready var item_title_label: Label = %ItemTitleLabel
 @onready var item_description_label: Label = %ItemDescriptionLabel
+@onready var buy_button: Button = %BuyButton
 
 
 
@@ -13,3 +16,9 @@ func _ready() -> void:
 	item_title_label.text = item.title
 	item_description_label.text = item.get_description()
 	
+func _process(delta: float) -> void:
+	if player_data.coins < item.price:
+		buy_button.disabled = true
+	else:
+		buy_button.disabled = false
+		
--- scripts/singletones/autoloads/globals.gd
+++ scripts/singletones/autoloads/globals.gd
@@ -5,5 +5,6 @@ var player_data: PlayerData
 var default_player_data = load('res://resources/player_data/default_player_data.tres')
 
 func get_player_data() -> PlayerData:
+	# @OPTIMIZE: not load every time?
 	SaveManager.load_player_data()
 	return player_data
