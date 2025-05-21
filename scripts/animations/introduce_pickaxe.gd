7917e093374283c3987a38f4f36170a4d58591eb
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-05-21 08:59:15 +0500

    [introduce_pickaxe]: moved from `new_block` to `new_resource`

--- scripts/animations/introduce_pickaxe.gd
+++ scripts/animations/introduce_pickaxe.gd
@@ -7,9 +7,10 @@ extends Node2D
 
 @onready var pickaxe_sprite: Sprite2D = %PickaxeSprite
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
-@onready var new_blocks_container: HBoxContainer = %NewBlocksContainer
+@onready var new_resources_container: HBoxContainer = %NewResourcesContainer
 
-var new_block_preload = preload("res://scenes/animations/new_block.tscn")
+
+var new_resource_preload = preload("res://scenes/animations/new_resource.tscn")
 
 func get_tier_blocks():
 	return block_dict.blocks.filter((func(b: BlockData): return b.tier == player_data.tier))
@@ -21,9 +22,9 @@ func _ready() -> void:
 	var tier_blocks: Array[BlockData] = get_tier_blocks()
 	
 	for block in tier_blocks:
-		var new_block = new_block_preload.instantiate()
-		new_block.block_texture = block.texture
-		new_blocks_container.add_child(new_block)
+		var new_resource = new_resource_preload.instantiate()
+		new_resource.resource_texture = block.drop_item.texture
+		new_resources_container.add_child(new_resource)
 	 
 	self.position = Vector2(get_viewport_rect().size.x / 2, get_viewport_rect().size.y / 2)
 	animation_player.play("introduce")
--- scripts/animations/new_block.gd
+++ scripts/animations/new_block.gd
@@ -1,11 +1,11 @@
 extends PanelContainer
 
-@export var block_texture: Texture2D
+@export var resorce_texture: Texture2D
 
-@onready var new_block_texture: TextureRect = %NewBlockTexture
+@onready var new_resource_texture: TextureRect = %NewResourceTexture
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 
 func _ready() -> void:
-	new_block_texture.texture = block_texture
+	new_resource_texture.texture = resorce_texture
 	animation_player.play("appear")
