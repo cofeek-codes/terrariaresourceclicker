88ce981728f566c6b12da65b1d530a0035a2dbef
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-06-28 21:38:29 +0500

    [buffs]: `Buff` resource

---
+++ resources/buffs/buff.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name Buff
+
+@export var item: ActiveItem
+
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var duration: int
+
+
+func get_tooltip():
+	return "%s (x%d) - %s" % [item.item.title, item.amount, item.item.get_description()]
