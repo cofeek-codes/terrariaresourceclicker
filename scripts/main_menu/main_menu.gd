235a29bacca455e517d55f95b603c88fca763be0
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-07-18 16:18:57 +0500

    [main_menu]: refactoring

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var menu_container: VBoxContainer = %MenuContainer
-@onready var menu_audio_stream_player: AudioStreamPlayer = %MenuAudioStreamPlayer
+@onready var audio_player: AudioStreamPlayer = %AudioPlayer
 
 var game_scene_preload: PackedScene = preload("res://scenes/game.tscn")
 var settings_scene_preload: PackedScene = preload("res://scenes/main_menu/settings.tscn")
@@ -18,7 +18,7 @@ func _ready() -> void:
 func _on_mouse_entered(button: Button):
 	button.pivot_offset = Vector2(button.size.x / 2, 0)
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
-	menu_audio_stream_player.play()
+	audio_player.play()
 	tween.tween_property(button, "scale", Vector2(1.2, 1.2), 0.3)
 
 
