2a280aadfcbe0241a3842fbef4ca67e4aab98bfa
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-07-23 20:35:06 +0500

    [mobs]: dynamic coloring of slimes

--- scripts/mobs/slimes/red_slime.gd
+++ scripts/mobs/slimes/red_slime.gd
@@ -5,6 +5,9 @@ class_name MobSlime
 
 func _ready() -> void:
 	super._ready()
-	#await get_tree().create_timer(1).timeout
-	#_take_damage(35)
+	
+	mob_data = mob_data as SlimeMobData
+	
+	sprite_animation_player.self_modulate = mob_data.color
+	mob_hit_particles.process_material.color = mob_data.color
 	print(mob_data.to_dict())
