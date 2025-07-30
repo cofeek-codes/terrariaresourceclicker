ce35f6611f21d2516bc658c20bc9d9e0059a5d90
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-07-30 23:29:27 +0500

    [shop]: shop_items now filled through script array

--- scripts/shop/shop.gd
+++ scripts/shop/shop.gd
@@ -1,8 +1,18 @@
 extends Control
 
-@onready var shop_items: VBoxContainer = %ShopItems
+@onready var shop_items_container: VBoxContainer = %ShopItems
+
+var item_scene_preload = preload("res://scenes/shop/shop_item.tscn")
+
+var shop_items = [
+	preload("res://resources/shop_items/cooper_pickaxe/cooper_pickaxe.tres"),
+	preload("res://resources/shop_items/mining_potion/mining_potion.tres"),
+	preload("res://resources/shop_items/iron_pickaxe/iron_pickaxe.tres"),
+]
 
 
 func _ready() -> void:
-	# @TODO: fill up the store automatically based on resource files
-	pass
+	for item in shop_items:
+		var item_scene = item_scene_preload.instantiate()
+		item_scene.item = item
+		shop_items_container.add_child(item_scene)
