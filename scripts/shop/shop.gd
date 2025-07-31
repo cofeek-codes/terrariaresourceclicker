6c507fe28f5cfd5c7b662a4b46664c812b794c21
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-07-31 19:18:28 +0500

    [shop]: shop_items loaded from json

--- scripts/shop/shop.gd
+++ scripts/shop/shop.gd
@@ -4,15 +4,18 @@ extends Control
 
 var item_scene_preload = preload("res://scenes/shop/shop_item.tscn")
 
-var shop_items = [
-	preload("res://resources/shop_items/cooper_pickaxe/cooper_pickaxe.tres"),
-	preload("res://resources/shop_items/mining_potion/mining_potion.tres"),
-	preload("res://resources/shop_items/iron_pickaxe/iron_pickaxe.tres"),
-]
+var shop_items: Array[ShopItem]
+
+const SHOP_ITEMS_PATH: String = "res://resources/shop_items/shop_items.json"
 
 
 func _ready() -> void:
+	_load_shop_items()
 	for item in shop_items:
 		var item_scene = item_scene_preload.instantiate()
 		item_scene.item = item
 		shop_items_container.add_child(item_scene)
+
+
+func _load_shop_items():
+	Globals.load_json_array(SHOP_ITEMS_PATH, shop_items)
