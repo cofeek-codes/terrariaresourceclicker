657a848c373b904d88cdd5e2583529919342ad5b
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-08-22 01:27:16 +0500

    [cloudsave]: new cloudsave init

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var menu_container: VBoxContainer = %MenuContainer
 @onready var audio_player: AudioStreamPlayer = %AudioPlayer
+@onready var cloud_save_button: Button = %CloudSaveButton
 
 var game_scene_preload: PackedScene = preload("res://scenes/game.tscn")
 var settings_scene_preload: PackedScene = preload("res://scenes/main_menu/settings.tscn")
@@ -11,12 +12,16 @@ var leaderboards_scene_preload: PackedScene = preload("res://scenes/leaderboards
 func _ready() -> void:
 	_init_locale()
 	SaveManager.load_settings()
-	for child in menu_container.get_children():
-		if child is Button:
-			child.mouse_entered.connect(_on_mouse_entered.bind(child))
-			child.mouse_exited.connect(_on_mouse_exited.bind(child))
+	for btn in get_tree().get_nodes_in_group("menu_buttons"):
+		btn.mouse_entered.connect(_on_mouse_entered.bind(btn))
+		btn.mouse_exited.connect(_on_mouse_exited.bind(btn))
 
 	Bridge.platform.send_message(Bridge.PlatformMessage.GAME_READY)
+
+
+func _set_cloudsave_btn_text():
+	var s = "%s: %s" % [tr("MENU_CLOUDSAVE"), tr("ON") if Globals.cloudsave_enabled else tr("OFF")]
+	cloud_save_button.text = s
 
 
 func _init_locale():
@@ -46,3 +51,7 @@ func _on_settings_button_pressed() -> void:
 
 func _on_leaderboards_button_pressed() -> void:
 	get_tree().change_scene_to_packed(leaderboards_scene_preload)
+
+
+func _on_cloud_save_button_pressed() -> void:
+	_set_cloudsave_btn_text()
