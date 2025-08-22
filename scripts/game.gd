0f131a452eae9c3037ce1ced50a9eeb50f1d3f70
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-08-22 17:50:51 +0500

    [game]: unneeded save options removed

--- scripts/game.gd
+++ scripts/game.gd
@@ -17,15 +17,9 @@ var on_before_unload_callback: JavaScriptObject
 
 
 func _ready() -> void:
-	get_tree().auto_accept_quit = false
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	print("coins loaded from file: %s" % player_data.coins.toAA(true))
 	handle_mouse_hover_ui_elements()
-	if OS.has_feature("web"):
-		on_before_unload_callback = JavaScriptBridge.create_callback(_on_before_unload)
-		var web_window = JavaScriptBridge.get_interface("window")
-		print_debug(web_window)
-		web_window.onbeforeunload = on_before_unload_callback
 
 	Bridge.platform.send_message(Bridge.PlatformMessage.GAMEPLAY_STARTED)
 
@@ -39,23 +33,6 @@ func _process(delta: float) -> void:
 func _on_save_timer_timeout() -> void:
 	print("save_timer timeout")
 	SaveManager.save_player_data()
-
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_WM_CLOSE_REQUEST:
-		print("about to exit...")
-		SaveManager.save_player_data()
-		get_tree().quit()
-	elif what == NOTIFICATION_APPLICATION_PAUSED:
-		SaveManager.save_player_data()
-
-
-func _on_before_unload(args):
-	var js_event = args[0]
-	print_debug("beforeunload happend in js")
-	SaveManager.save_player_data()
-	js_event.preventDefault()
-	js_event.returnValue = ""
 
 
 func ui_mouse_entered():
