fe530def8d26d793f1148f3a7f9040bd886adddd
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-08-23 19:15:45 +0500

    [settings]: added new utility functions

--- resources/settings/settings.gd
+++ resources/settings/settings.gd
@@ -5,3 +5,18 @@ class_name Settings
 @export var master_volume: float = 1.0
 @export var music_volume: float = 1.0
 @export var sound_volume: float = 1.0
+
+
+func to_dict():
+	var dict = {}
+	for prop in get_property_list():
+		if prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+			var name = prop.name
+			dict[name] = get(name)
+	return dict
+
+
+func from_dict(dict: Dictionary):
+	master_volume = dict["master_volume"]
+	music_volume = dict["music_volume"]
+	sound_volume = dict["sound_volume"]
