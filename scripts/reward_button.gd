bc2bb579707bca29ec6b83fca500b59df7484c4b
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-08-29 04:39:58 +0500

    [reward_button]: reward button (without actual reward intergration yet)

---
+++ scripts/reward_button.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var player_data: PlayerData = Globals.get_player_data()
+
+@onready var button: Button = %Button
+@onready var time_label: Label = %TimeLabel
+@onready var cooldown_timer: Timer = %CooldownTimer
+
+
+func _ready() -> void:
+	time_label.hide()
+
+
+func _process(delta: float) -> void:
+	time_label.text = str(int(cooldown_timer.time_left))
+
+
+func _on_button_pressed() -> void:
+	player_data.coins_per_click.multiplyEquals(2)
+	player_data.coins_per_second.multiplyEquals(2)
+	button.disabled = true
+	cooldown_timer.start()
+	time_label.show()
+
+
+func _on_cooldown_timer_timeout() -> void:
+	player_data.coins_per_click.divideEquals(2)
+	player_data.coins_per_second.divideEquals(2)
+	button.disabled = false
+	time_label.hide()
