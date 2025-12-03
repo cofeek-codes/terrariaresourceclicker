53e0399e30262dc8a88d65c142b9fa761d043d92
Cofeek <kormyshev11@mail.ru>
2025-12-03 15:34:54 +0500

    [build_script]: added `--headless` flag to godot to prevent from opening editor on export

--- build_prod.sh
+++ build_prod.sh
@@ -4,5 +4,5 @@ GODOT_PATH="../Godot_v4.5-stable_win64.exe"
 EXPORT_PATH="../terrariaresourceclicker_exports/web"
 
 rm -rf $EXPORT_PATH/** &&
-$GODOT_PATH --export-release Playgama $EXPORT_PATH/index.html &&
+$GODOT_PATH --headless --export-release Playgama $EXPORT_PATH/index.html &&
 7z a -tzip $EXPORT_PATH/web.zip $EXPORT_PATH/**
