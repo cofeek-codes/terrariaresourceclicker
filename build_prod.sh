9462d9bf98dec278530ce686097e3f8210b205d0
Cofeek <kormyshev11@mail.ru>
2025-12-22 03:54:05 +0500

    [build_script]: build script improvement

--- build_prod.sh
+++ build_prod.sh
@@ -3,6 +3,16 @@
 GODOT_PATH="../Godot_v4.5-stable_win64.exe"
 EXPORT_PATH="../terrariaresourceclicker_exports/web"
 
-rm -rf $EXPORT_PATH/** &&
+echo " "
+echo " "
+echo " "
+
+rm -rf -v $EXPORT_PATH/** &&
+
+echo " "
+echo " "
+echo " "
+
 $GODOT_PATH --headless --export-release Playgama $EXPORT_PATH/index.html &&
+
 7z a -tzip $EXPORT_PATH/web.zip $EXPORT_PATH/**
